ac7d9896c04f14939d27b28ae5f660e89e507721
Omar Bassyouni <omarbassyouni991@gmail.com>
2020-06-13 23:00:14 +0200

    add timer logic

--- CardAppearance/Use Cases/CreateSecondsTimerUseCase.swift
+++ CardAppearance/Use Cases/CreateSecondsTimerUseCase.swift
@@ -19,22 +19,20 @@ final class CreateSecondsTimerUseCase: CreateSecondsTimerUseCaseInterface {
     private let timerSubject = PublishSubject<Int>()
     private let bag = DisposeBag()
     private var timerDisposable: Disposable?
+    private let scheduler: SchedulerType
+    
+    private var _oneSecondTimer: Observable<Int> {
+        return Observable<Int>.interval(.seconds(1), scheduler: scheduler)
+    }
     
     var secondsObservable: Observable<Int> {
-        return timerSubject.asObservable()
+        _oneSecondTimer
+            .map { $0 % 12 }
+            .map { $0 + 1 }
     }
     
     // MARK: - Initializers
-    init() {
-        startTimer()
-    }
-    
-    private func startTimer() {
-        timerDisposable = Observable<Int>
-            .timer(.seconds(0), period: .seconds(1), scheduler: MainScheduler.instance)
-            .map { $0 + 1 }
-            .subscribe(onNext: { [weak self] (second) in
-                self?.timerSubject.onNext(second)
-            })
+    init(scheduler: SchedulerType) {
+        self.scheduler = scheduler
     }
 }
--- CardAppearanceTests/Use Cases/CreateSecondsTimerUseCaseTests.swift
+++ CardAppearanceTests/Use Cases/CreateSecondsTimerUseCaseTests.swift
@@ -8,23 +8,58 @@
 
 import RxSwift
 import RxBlocking
+import RxTest
 import XCTest
+import RxCocoa
 @testable import CardAppearance
 
 class CreateSecondsTimerUseCaseTests: XCTestCase {
     
     var sut: CreateSecondsTimerUseCase!
+    var bag: DisposeBag!
     
     override func setUp() {
-        sut = CreateSecondsTimerUseCase()
+        bag = DisposeBag()
+        sut = getSutWithScheduler(scheduler: MainScheduler.instance)
     }
     
     override func tearDown() {
         sut = nil
+        bag = nil
+    }
+    
+    private func getSutWithScheduler(scheduler: SchedulerType) -> CreateSecondsTimerUseCase {
+        return CreateSecondsTimerUseCase(scheduler: scheduler)
     }
     
     func testSecondsObservable_initialState_equalsOne() throws {
-        XCTAssertEqual(try sut.secondsObservable.toBlocking().first(), 1)
+        XCTAssertEqual(try sut.secondsObservable.take(1).toBlocking().first(), 1)
+    }
+    
+    func testSecondsObservable_whenReachesTwelve_resetsBackToOne() {
+        let scheduler = TestScheduler(initialClock: 0)
+        
+        let res = scheduler.start(created: 0, subscribed: 0, disposed: 14) {
+            self.getSutWithScheduler(scheduler: scheduler).secondsObservable
+        }
+        
+        let correctSequence = [
+            Recorded.next(1, 1),
+            Recorded.next(2, 2),
+            Recorded.next(3, 3),
+            Recorded.next(4, 4),
+            Recorded.next(5, 5),
+            Recorded.next(6, 6),
+            Recorded.next(7, 7),
+            Recorded.next(8, 8),
+            Recorded.next(9, 9),
+            Recorded.next(10, 10),
+            Recorded.next(11, 11),
+            Recorded.next(12, 12),
+            Recorded.next(13, 1),
+        ]
+        
+        XCTAssertEqual(res.events, correctSequence)
     }
 
 }
