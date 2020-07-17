5f7951d6d29d839593e4d054a3aa747fe6cd8b39
Omar Bassyouni <omarbassyouni991@gmail.com>
2020-07-18 00:49:23 +0200

    add convince methods for adding Constraints

--- CardAppearance/Extensions/UIView+Extension.swift
+++ CardAppearance/Extensions/UIView+Extension.swift
@@ -14,3 +14,95 @@ extension UIView {
     }
 }
 
+extension UIView {
+    func anchor(top: NSLayoutYAxisAnchor? = nil,
+                leading: NSLayoutXAxisAnchor? = nil,
+                bottom: NSLayoutYAxisAnchor? = nil,
+                trailing: NSLayoutXAxisAnchor? = nil,
+                padding: UIEdgeInsets = .zero,
+                size: CGSize = .zero) {
+        
+        translatesAutoresizingMaskIntoConstraints = false
+        var constraints = [NSLayoutConstraint]()
+        
+        if let top = top {
+            constraints.append(topAnchor.constraint(equalTo: top, constant: padding.top))
+        }
+        
+        if let leading = leading {
+            constraints.append(leadingAnchor.constraint(equalTo: leading, constant: padding.left))
+        }
+        
+        if let bottom = bottom {
+            constraints.append(bottomAnchor.constraint(equalTo: bottom, constant: -padding.bottom))
+        }
+        
+        if let trailing = trailing {
+            constraints.append(trailingAnchor.constraint(equalTo: trailing, constant: -padding.right))
+        }
+        
+        if size.width != 0 {
+            constraints.append(widthAnchor.constraint(equalToConstant: size.width))
+        }
+        
+        if size.height != 0 {
+            constraints.append(heightAnchor.constraint(equalToConstant: size.height))
+        }
+        
+        NSLayoutConstraint.activate(constraints)
+    }
+    
+    func fillSuperview(padding: UIEdgeInsets = .zero) {
+        translatesAutoresizingMaskIntoConstraints = false
+        if let superviewTopAnchor = superview?.topAnchor {
+            topAnchor.constraint(equalTo: superviewTopAnchor, constant: padding.top).isActive = true
+        }
+        
+        if let superviewBottomAnchor = superview?.bottomAnchor {
+            bottomAnchor.constraint(equalTo: superviewBottomAnchor, constant: -padding.bottom).isActive = true
+        }
+        
+        if let superviewLeadingAnchor = superview?.leadingAnchor {
+            leadingAnchor.constraint(equalTo: superviewLeadingAnchor, constant: padding.left).isActive = true
+        }
+        
+        if let superviewTrailingAnchor = superview?.trailingAnchor {
+            trailingAnchor.constraint(equalTo: superviewTrailingAnchor, constant: -padding.right).isActive = true
+        }
+    }
+    
+    func centerInSuperview(size: CGSize) {
+        translatesAutoresizingMaskIntoConstraints = false
+        if let superviewCenterXAnchor = superview?.centerXAnchor {
+            centerXAnchor.constraint(equalTo: superviewCenterXAnchor).isActive = true
+        }
+        
+        if let superviewCenterYAnchor = superview?.centerYAnchor {
+            centerYAnchor.constraint(equalTo: superviewCenterYAnchor).isActive = true
+        }
+        
+        widthAnchor.constraint(equalToConstant: size.width).isActive = true
+        heightAnchor.constraint(equalToConstant: size.height).isActive = true
+    }
+    
+    func equalWidthAndHeight(to view: UIView, multiplier: CGFloat) {
+        var constraints = [NSLayoutConstraint]()
+        constraints.append(NSLayoutConstraint(item: self,
+                                              attribute: .width,
+                                              relatedBy: .equal,
+                                              toItem: view,
+                                              attribute: .width,
+                                              multiplier: multiplier,
+                                              constant: 0))
+        
+        constraints.append(NSLayoutConstraint(item: self,
+                                              attribute: .height,
+                                              relatedBy: .equal,
+                                              toItem: view,
+                                              attribute: .height,
+                                              multiplier: multiplier,
+                                              constant: 0))
+        
+        NSLayoutConstraint.activate(constraints)
+    }
+}
